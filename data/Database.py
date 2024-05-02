f593a5c243ccb5b572bf024853958449fc5dee58
Marc Goodman <marc.goodman@pcc.edu>
2024-05-01 17:53:58 -0700

    Added some starter data.

--- data/Database.py
+++ data/Database.py
@@ -69,6 +69,16 @@ class Database:
         ps_btn = MusicVideo("Patti Smith", "Because the Night",
                             "https://www.youtube.com/watch?v=c_BcivBprM0", 1978,
                             "Written by Bruce and Patti")
+        kb_ruth = MusicVideo("Kate Bush", "Running up that Hill",
+                             "https://www.youtube.com/watch?v=wp43OdtAAkM&ab_channel=KateBushMusic", 1985,
+                             "Song surged in popularity due to its use in Stranger Things.")
+        kbpg_dgu = MusicVideo("Kate Bush and Peter Gabriel", "Don't Give Up",
+                              "https://www.youtube.com/watch?v=VjEq-r2agqc&ab_channel=PeterGabriel", 1986,
+                              "Video was very popular on MTV for a long time.")
+        pg_sh = PerformanceVideo("Peter Gabriel", "Solsbury Hill",
+                                 "https://www.youtube.com/watch?v=WeYqJxlSv-Y&ab_channel=PeterGabriel", 1977,
+                                 "Montage of live performances spanning 35 years.",
+                                 "Various locations", "1978-2013")
 
         bs = PlayList("Bruce Springsteen", [bs_bbl, bs_btn],
                       "https://glassgirder.com/music_player/images/bruce.jpg",
@@ -76,11 +86,18 @@ class Database:
         ps = PlayList("Patti Smith", [ps_btn],
                       "https://glassgirder.com/music_player/images/patti.jpg",
                       "Patti Smith")
-        all = PlayList(PlayList.ALL_VIDEOS, [bs_bbl, mm_bbl, bs_btn, ps_btn],
+        pg = PlayList("Peter Gabriel", [pg_sh, kbpg_dgu],
+                      "https://scontent.fhio2-2.fna.fbcdn.net/v/t1.6435-9/118160015_10158314269559760_6423978243084146145_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Qn6PPdcyNjMAb4f8ycC&_nc_ht=scontent.fhio2-2.fna&oh=00_AfDHmwc7zpKQomT3vtcycXL3xCXB-Sc4n9UhbeL5mvQ_2A&oe=66552280",
+                      "Formerly drummer for Genesis.")
+        kb = PlayList("Kate Bush", [kbpg_dgu, kb_ruth],
+                      "https://i.iheart.com/v3/surl/aHR0cDovL2ltYWdlLmloZWFydC5jb20vaW1hZ2VzL292ZXJyaWRlLzM3OTM2XzZlMzA0ZTBiLTkyMDUtNGI5My04YWU3LTczYWFhMGYyOWNiMi5qcGc=?ops=fit%28480%2C480%29%2Crun%28%22circle%22%29&sn=eGtleWJhc2UyMDIxMTExMDo6_8saG4AYSLrIWkuwdc_DolZpM7mBlOcsM5ALSb_88w%3D%3D&surrogate=1cOXl179JY-syhxYSCX6Q1a_Mcu6UO8d-F4oJzpZf1hcUbJr4aImw9gKF1etygNPvEf1drJMM41pDkyR0A231uXuml9Gz_YQA80xUi4rA7aMuVDGzKutY5QxBOSe18ws_xl8GBXkEEGxV0VwwaYHO7Z1bXq_rK2ysgUqcKI6YkDJq3dJeVT0mXQ3VuQBvjB_9u1dxeOlL5YsvsMp59C5RAMY1MPtumR0Ljm78eHVmW_LFPSU2VmfNuyt9VupNihj",
+                      "Discovered by David Gilmore of Pink Floyd.")
+
+        all = PlayList(PlayList.ALL_VIDEOS, [bs_bbl, mm_bbl, bs_btn, ps_btn, pg_sh, kbpg_dgu, kb_ruth],
                        "https://glassgirder.com/music_player/images/all_videos.jpg",
                        "All Videos")
 
-        return all, [bs, ps, all]
+        return all, [bs, ps, pg, kb, all]
 
     @classmethod
     def save_playlist(cls, playlist):
